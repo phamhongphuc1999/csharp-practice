72eba0363e66ffaaa4b1182608c78233a18c0c95
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-09-28 22:26:47 +0700

    fix: Custom List

--- CustomCollection/CustomList.cs
+++ CustomCollection/CustomList.cs
@@ -1,74 +1,77 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
 namespace CustomCollection
 {
-    class CustomList<T>
+    public class CustomList<T>: IEnumerable<T>
     {
-        private T[] items;
-        private int size;
-        private int capacity;
-        private int step;
+        private T[] _items;
+        private int _size;
+        private int _capacity;
+        private int _step;
 
         public CustomList()
         {
-            items = new T[10];
-            size = 0;
-            capacity = 10;
-            step = 10;
+            _items = new T[10];
+            _size = 0;
+            _capacity = 10;
+            _step = 10;
         }
 
         public CustomList(int capacity, int step)
         {
-            int _step = 10;
+            int _step = 1000;
             int _capacity = 10;
             if (step > 0) _step = step;
             if (capacity > 0) _capacity = capacity;
-            items = new T[_capacity];
-            this.capacity = _capacity;
-            this.step = _step;
-            size = 0;
+            this._capacity = _capacity;
+            this._step = _step;
+            _items = new T[this._capacity];
+            _size = 0;
         }
 
         public CustomList(int capacity)
         {
             int _capacity = 10;
             if (capacity > 0) _capacity = capacity;
-            items = new T[_capacity];
-            this.capacity = capacity;
-            this.step = 10;
-            size = 0;
+            this._capacity = _capacity;
+            _items = new T[this._capacity];
+            this._step = 10;
+            _size = 0;
         }
 
         public CustomList(IEnumerable<T> collection)
         {
             int count = collection.Count();
-            items.CopyTo(collection.ToArray(), count);
-            size = 0;
-            capacity = count;
+            _capacity = 10 * (count / 10) + 10;
+            _step = 10;
+            _items = new T[_capacity];
+            collection.ToArray().CopyTo(_items, 0);
+            _size = count;
         }
 
         public int Count
         {
-            get { return size; }
+            get { return _size; }
         }
 
         public int Capacity
         {
-            get { return capacity; } 
+            get { return _capacity; } 
             set
             {
-                if (value >= size) Array.Resize<T>(ref items, value);
+                if (value >= _size) Array.Resize<T>(ref _items, value);
             }
         }
 
         public int Step
         {
-            get { return step; }
+            get { return _step; }
             set
             {
-                if (value > 0) step = value;
+                if (value > 0) _step = value;
             }
         }
 
@@ -76,33 +79,79 @@ namespace CustomCollection
         {
             get
             {
-                if (index >= 0 && index < size)
-                    return items[index];
+                if (index >= 0 && index < _size)
+                    return _items[index];
                 else throw new IndexOutOfRangeException();
             }
             set
             {
-                if (index >= 0 && index < size)
-                    items[index] = value;
+                if (index >= 0 && index < _size)
+                    _items[index] = value;
                 else throw new IndexOutOfRangeException();
             }
         }
 
         public void Add(T item)
         {
-            if (size < capacity) items[size++] = item;
+            if (_size < _capacity) _items[_size++] = item;
             else
             {
-                Array.Resize<T>(ref items, this.Count + this.Step);
-                items[size++] = item;
+                _capacity += _step;
+                Array.Resize<T>(ref _items, _capacity);
+                _items[_size++] = item;
             }
         }
 
         public bool Contain(T item)
         {
-            foreach (T x in items)
+            foreach (T x in _items)
                 if (item.Equals(x)) return true;
             return false;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return (IEnumerator<T>)new CustomListtEnumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class CustomListtEnumerator : IEnumerator<T>
+        {
+            private CustomList<T> list;
+            private int index;
+
+            public CustomListtEnumerator(CustomList<T> list)
+            {
+                this.list = list;
+                index = -1;
+            }
+
+            public T Current
+            {
+                get { return list[index]; }
+            }
+
+            object IEnumerator.Current => throw new NotImplementedException();
+
+            public void Dispose()
+            {
+            }
+
+            public bool MoveNext()
+            {
+                index++;
+                if (index >= list.Count) return false;
+                return true;
+            }
+
+            public void Reset()
+            {
+                index = -1;
+            }
         }
     }
 }
