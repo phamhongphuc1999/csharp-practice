b2536bf298154b041fbe43f3259ef90343a0e204
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2020-10-17 11:28:17 +0700

    add: Compare

---
+++ MyLibrary/ArangeAlgorithm/Config.cs
@@ -0,0 +1,12 @@
+﻿namespace MyLibrary.ArangeAlgorithm
+{
+    public class Config
+    {
+        public enum QuickSortPivot
+        {
+            HEADER = 0,
+            END = 1,
+            MIDDLE = 2
+        }
+    }
+}
---
+++ MyLibrary/ArangeAlgorithm/CoreAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLibrary.ArangeAlgorithm
+{
+    public static partial class CoreAlgorithm
+    {
+        public static IEnumerable<T> BubbleSort<T>(this IEnumerable<T> source, Func<T, T, bool> comparer)
+        {
+            List<T> arr = source.ToList();
+            int count = source.Count();
+            for (int i = 0; i < count - 1; i++)
+            {
+                for (int j = i + 1; j < count; j++)
+                {
+                    if (!comparer(arr[i], arr[j]))
+                    {
+                        T temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return source;
+        }
+
+        //public static IEnumerable<T> QuickSort<T>(this IEnumerable<T> source, Func<T, T, bool> comparer, Config.QuickSortPivot pivot)
+        //{
+
+        //}
+    }
+}
